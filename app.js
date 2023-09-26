4e3b804e7b78c8e467af73a30e688efd963e8d8c
Jude_Savio <judealphonsesavio@gmail.com>
2023-09-27 03:38:29 +1000

    Fully functioning submit page
    -> Added middleware to restrict access to /submit URL from the address bar.
    
    ->  MailTrap synchronisation with contact page done.

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ dotenv.config();
 app.use(express.static(__dirname + '/public')); 
 app.use(express.urlencoded({ extended: true }));
 
+// Custom middleware to protect the /submit route
+app.use('/submit', (req, res, next) => {
+  // Check if the request originated from a form submission
+  if (req.headers['referer'] && req.headers['referer'].includes('/contact')) {
+    // The request came from the /contact page, proceed to the /submit route
+    next();
+  } else {
+    // Redirect or return an error response to prevent access
+    res.status(403).send('Access to /submit is not allowed.');
+  }
+});
+
 app.get('/home', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -17,42 +29,52 @@ app.get('/contact', (req, res) => {
   res.sendFile(__dirname + '/views/contact.html');
 });
 
+app.get('/submit', (req, res) => {
+  res.sendFile(__dirname + '/views/submit.html');
+});
+
+
+
 app.post('/submit', (req, res) => {
 
-    const { firstName, lastName, email, message } = req.body;
-      
-    // Use Nodemailer to send an email
-    var transport = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
-      auth: {
-        user: process.env.MAILUSER,
-        pass: process.env.MAILPASS
-      }
-    });
-  
-    mailBody = message
-    fromEmail = email
-    mailSubject = 'Contact Request recieved from ' + firstName + ' ' + lastName
-  
-    const mailOptions = {
-      from: fromEmail,
-      to: 'team.encephalon@gmail.com',
-      subject: mailSubject,
-      text: mailBody,
-    };
-  
-    transport.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(error);
-        res.send('Error sending the email');
-      } else {
-        console.log('Email sent: ' + info.response);
-        res.send('Email sent successfully');
-      }
-    });
+  const { firstName, lastName, email, message } = req.body;
     
+  // Use Nodemailer to send an email
+  var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: process.env.MAILUSER,
+      pass: process.env.MAILPASS
+    }
   });
+
+  mailBody = message
+  fromEmail = email
+  mailSubject = 'Contact Request recieved from ' + firstName + ' ' + lastName
+
+  const mailOptions = {
+    from: fromEmail,
+    to: 'team.encephalon@gmail.com',
+    subject: mailSubject,
+    text: mailBody,
+  };
+
+  transport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.send('Error sending the email');
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+
+    //Redirect to the submit page once finished.
+    res.redirect('/submit');
+
+  });
+
+  });
+
   
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ public/js/handler/submit-handler.js
@@ -0,0 +1,19 @@
+// JavaScript code to handle the countdown and redirection
+let countdown = 25; // Initial countdown time in seconds
+
+// Function to update the countdown timer and handle redirection
+function updateCountdown() {
+  const countdownSpan = document.getElementById('countdown');
+  countdownSpan.textContent = countdown;
+
+  if (countdown <= 0) {
+    // Redirect to the desired URL after the countdown reaches 0
+    window.location.href = '/home';
+  } else {
+    countdown--; // Decrement the countdown timer
+    setTimeout(updateCountdown, 1000); // Update every second (1000 ms)
+  }
+}
+
+// Start the countdown
+updateCountdown();
